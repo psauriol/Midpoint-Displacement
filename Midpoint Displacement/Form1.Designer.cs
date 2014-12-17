e81137ae6780f763014813c4f0cb6568fd9b6985
psauriol <psauriol@yahoo.com>
2014-12-17 14:22:12 -0500

    Midpoint line displacement
    Creates a small terrain using midpoint line displacement

---
+++ Midpoint Displacement/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Midpoint_Displacement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ShowlinesCheckbox = new System.Windows.Forms.CheckBox();
+            this.ShowFillCheckbox = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(998, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Debug1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Debug2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1070, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(911, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Number of generations";
+            // 
+            // ShowlinesCheckbox
+            // 
+            this.ShowlinesCheckbox.AutoSize = true;
+            this.ShowlinesCheckbox.Location = new System.Drawing.Point(998, 51);
+            this.ShowlinesCheckbox.Name = "ShowlinesCheckbox";
+            this.ShowlinesCheckbox.Size = new System.Drawing.Size(172, 21);
+            this.ShowlinesCheckbox.TabIndex = 5;
+            this.ShowlinesCheckbox.Text = "Show Calculated Lines";
+            this.ShowlinesCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // ShowFillCheckbox
+            // 
+            this.ShowFillCheckbox.AutoSize = true;
+            this.ShowFillCheckbox.Checked = true;
+            this.ShowFillCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ShowFillCheckbox.Location = new System.Drawing.Point(998, 78);
+            this.ShowFillCheckbox.Name = "ShowFillCheckbox";
+            this.ShowFillCheckbox.Size = new System.Drawing.Size(85, 21);
+            this.ShowFillCheckbox.TabIndex = 6;
+            this.ShowFillCheckbox.Text = "Show Fill";
+            this.ShowFillCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1113, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Erase";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 555);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ShowFillCheckbox);
+            this.Controls.Add(this.ShowlinesCheckbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Displacement test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox ShowlinesCheckbox;
+        private System.Windows.Forms.CheckBox ShowFillCheckbox;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Midpoint Displacement/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Midpoint_Displacement
+{
+    public partial class Form1 : Form
+    {
+        Point leftPoint = new Point(0, 0);
+        Point rightPoint = new Point(1200, 0);
+        Color awsome = Color.Black;//color parts
+        int A;
+        int R;
+        int G;
+        int B;
+        //string txtColor;
+        //Random rand = new Random();
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "11";
+            positionAdjust();
+        }
+
+        void positionAdjust()
+        {
+            leftPoint.X += 100;
+            leftPoint.Y += 400;
+            rightPoint.X -= 100;
+            rightPoint.Y += 400;
+        }
+
+       /* void curveCalc(Point startPoint, Point endPoint, int stopCalc)
+        {
+            System.Drawing.Graphics graphicsObj;
+            graphicsObj = this.CreateGraphics();
+            Pen myPen = new Pen(awsome, 1);
+            if (stopCalc > 0)
+            {
+                ChangeColor();
+
+                if (startPoint.Y < 300)
+                {
+                startPoint.X += 10;
+                endPoint.Y -= 10;
+                curveCalc(startPoint, endPoint, stopCalc - 1);
+                graphicsObj.DrawLine(myPen, startPoint, endPoint);
+                }else{
+
+            
+                    endPoint.X -= 10;
+                    startPoint.Y -= 10;
+                    curveCalc(startPoint, endPoint, stopCalc - 1);
+                    graphicsObj.DrawLine(myPen, startPoint, endPoint);
+                }
+            }
+        }*/
+       /* int randomOffsetY(int offset)
+        {
+                //start randomization block
+                int add = new Random().Next(1, 6);
+                add /= 10;
+                int bob = (new Random().Next(11, 21)) % 2;
+                if (bob == 0)
+                {
+                    bob = -1;
+                }
+
+                offset *= (1 + add) * bob;
+                //end random block    
+
+            return offset;
+        }*/
+
+
+        void lineCalc(Point startPoint, Point endPoint, int stopCalc, int offset)
+        {
+            
+            offset /= 2;
+
+
+            if (stopCalc > 0)
+            {
+               
+                
+                Point midPoint = new Point();
+
+                midPoint.X = ((startPoint.X + endPoint.X) / 2);
+                midPoint.Y = ((startPoint.Y + endPoint.Y) / 2) - offset;
+                ChangeColor();
+                System.Drawing.Graphics graphicsObj;
+                graphicsObj = this.CreateGraphics();
+                Pen leftPen = new Pen(awsome, 1);//pen/color for left hand lines
+                Pen rightPen = new Pen(Color.Red, 1); //pen/color for right hand lines
+                Pen basePen = new Pen(Color.ForestGreen, 1);//pen/color for terrain layer
+                Pen waterPen = new Pen(Color.Blue, 1);//pen/color for water layer
+                Pen snowPen = new Pen(Color.WhiteSmoke, 1);//pen/color for snow layer
+                Pen skyPen = new Pen(Color.LightSkyBlue, 1);//pen/color for sky layer
+
+                
+                if (stopCalc == 1 || ShowlinesCheckbox.Checked == true)
+                {
+
+                    if (ShowFillCheckbox.Checked == true)
+                    {
+                        graphicsObj.DrawLine(skyPen, midPoint.X, midPoint.Y, midPoint.X, 50);//draws right hand lines
+                        if (midPoint.Y < 500)
+                        {
+                          
+                            graphicsObj.DrawLine(basePen, midPoint.X, midPoint.Y, midPoint.X, 500);//fills between terrain and base
+                            
+                            if (midPoint.Y < 200)
+                            {
+                                int snowLine = new Random().Next(195, 205);
+                                graphicsObj.DrawLine(snowPen, midPoint.X, midPoint.Y, midPoint.X, snowLine);//fills between snow layer and peak
+                            }
+                        }
+                        if(midPoint.Y > 400)
+                        {
+                            graphicsObj.DrawLine(waterPen, midPoint.X, midPoint.Y, midPoint.X, 400);//fills stuff under base with blue
+                        }
+                    }
+
+                    
+                    graphicsObj.DrawLine(rightPen, midPoint, endPoint);//draws right hand lines
+                    graphicsObj.DrawLine(leftPen, startPoint, midPoint);//draws left hand lines
+
+                }
+
+                
+                //start randomization block
+                //randomOffsetY(offset);
+                int add = new Random().Next(1, 8);
+                add /= 10;
+                int bob = (new Random().Next(11, 21)) % 2;
+                if (bob == 0)
+                {
+                    bob = -1;
+                }
+
+                offset *= (1 + add) * bob;//kill this line to remove the displacement
+                //end random block    
+
+                lineCalc(startPoint, midPoint, stopCalc - 1, offset);//starts method to calculate new lefthand line
+                lineCalc(midPoint, endPoint, stopCalc - 1, offset);//starts method to calculate new righthand line
+
+
+                
+            }
+        }
+        void ChangeColor()
+        {
+            //convert color to string
+            string txtColor = awsome.A + "," + awsome.R + "," + awsome.G + "," + awsome.B;
+
+            
+            //to convert your string to color
+            string[] awsomeSplit = txtColor.Split(',');
+            A = awsome.A;
+            R = awsome.R;
+            G = awsome.G;
+            B = awsome.B;
+            if ((R + 5) < 255)
+            {
+                R = 240;
+               // R += 1;
+            }
+            else if ((G + 5) < 255)
+            {
+                G += 1;
+            }
+            else if ((B + 5) < 255)
+            {
+                B += 1;
+            }
+            else { }
+            A = 200;
+            awsomeSplit[0] = Convert.ToString(A);
+            awsomeSplit[1] = Convert.ToString(R);
+            awsomeSplit[2] = Convert.ToString(G);
+            awsomeSplit[3] = Convert.ToString(B);
+            awsome = Color.FromArgb(Convert.ToInt32(awsomeSplit[0]),
+                                Convert.ToInt32(awsomeSplit[1]),
+                                Convert.ToInt32(awsomeSplit[2]),
+                                Convert.ToInt32(awsomeSplit[3]));
+
+            
+            label2.Text = txtColor;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int times = Convert.ToInt32(textBox1.Text);
+            
+            int peakY = new Random().Next(256, 512);//determins starting height of peak
+    
+
+
+            lineCalc(leftPoint, rightPoint, times, peakY);
+          //debug label1.Text = "Point 1: " + leftPoint + "\nPoint 2: " + rightPoint + "\nMid-Point: " ;
+          //debug  label2.Text = "" + awsome;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+    }
+}
